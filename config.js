4f281839101d39e540b0464509b27ee32c37637c
سید <seyyedmohammadtahaghadami@gmail.com>
2026-08-11 21:20:55 +0330

    Delete config.js

--- config.js
+++
@@ -1,3 +0,0 @@
-const saved = localStorage.getItem('typo_api_base');
-export const API_BASE_URL = saved || (location.protocol === 'http:' || location.protocol === 'https:' ? location.origin : '');
-export function setApiBase(value){ if(value) localStorage.setItem('typo_api_base', value.replace(/\/$/,'')); else localStorage.removeItem('typo_api_base'); }
